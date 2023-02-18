5a78c97872742bd69c09094484c4419387ca5b5b
leminol <srovburak@gmail.com>
2023-02-18 19:56:47 +0300

    .

--- src/loader.js
+++ src/loader.js
@@ -5,7 +5,7 @@ function includeHTML() {
     for (i = 0; i < z.length; i++) {
       elmnt = z[i];
       /*search for elements with a certain attribute:*/
-      file = elmnt.getAttribute("w3-include-html");
+      file = elmnt.getAttribute("include-html");
       if (file) {
         /*make an HTTP request using the attribute value as the file name:*/
         xhttp = new XMLHttpRequest();
